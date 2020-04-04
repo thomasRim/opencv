e4125697d11f51e575ab8dcb7bb2463a3d9957d1
Volodymyr Yevdokymov <vevdokimov@geeksforless.net>
2020-04-04 22:59:28 +0300

    new images. add config

--- yolo_object_detection/yolo_object_detection_image.py
+++ yolo_object_detection/yolo_object_detection_image.py
@@ -10,28 +10,30 @@ from datetime import datetime
 # Net
 sysPath = os.path.dirname(os.path.abspath(__file__))
 
-weights = os.path.join(sysPath, 'yolov3-custom.weights')
-config = os.path.join(sysPath, 'yolov3-custom.cfg')
-names = os.path.join(sysPath, 'custom.names')
+weights = os.path.join(sysPath, 'lib/yolov3-custom.weights')
+config = os.path.join(sysPath, 'lib/yolov3-custom.cfg')
+names = os.path.join(sysPath, 'lib/custom.names')
 
-sources = ['sources/real_1.png', 'sources/real_2.png']#['sources/washer_1.jpeg', 'sources/washer_2.jpeg', 'sources/washer_3.jpeg', 'sources/washer_4.jpeg', 'sources/washer_5.jpeg', 'sources/washer_6.jpeg']
+# ['sources/washer_1.jpeg', 'sources/washer_2.jpeg', 'sources/washer_3.jpeg', 'sources/washer_4.jpeg', 'sources/washer_5.jpeg', 'sources/washer_6.jpeg']
+sources = ['sources/rs-1.jpg', 'sources/rs-2.png', 'sources/rs-3.png']
 
 yo = yolo.Yolo(weights, config, names)
 
-for (i,name) in  enumerate(sources):
+for (i, name) in enumerate(sources):
 
-        # Loading image
-        imagePath = os.path.join(sysPath, name)
-        utils.fileExist(imagePath)
+    # Loading image
+    imagePath = os.path.join(sysPath, name)
+    if utils.fileExist(imagePath):
         img = cv2.imread(imagePath)
-        img = cv2.resize(img, None, fx=2.4, fy=2.4)
+        # img = cv2.resize(img, None, fx=2.4, fy=2.4)
 
         yo.detectFrom(img)
 
         # imageName = os.path.join(sysPath, 'result/image_' + str(int(datetime.timestamp(datetime.now()))) + '.jpg')
         # cv2.imwrite(imageName, img)
 
-        cv2.imshow("Image"+str(i), img)
+        cv2.imshow("Image" + str(i), img)
+
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
